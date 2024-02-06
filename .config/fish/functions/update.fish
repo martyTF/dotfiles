da8f958536ed6dccdaa56b6a6e15cf27c9818d0d
martyTF <marty_TF@protonmail.com>
2024-02-06 10:05:44 +0100

    h7

--- .config/fish/functions/update.fish
+++ .config/fish/functions/update.fish
@@ -1,3 +1,7 @@
 function update
-sudo dnf update rpmfusion-nonfree-release rpmfusion-free-release fedora-repos nobara-repos --refresh && sudo dnf distro-sync --refresh && sudo dnf update --refresh && flatpak update -y && am update
+sudo dnf update -y rpmfusion-nonfree-release rpmfusion-free-release fedora-repos nobara-repos --refresh && sudo dnf distro-sync --refresh
+sudo dnf update -y --refresh
+flatpak update -y
+am update
+doom sync
 end
