ad5d46a3204fdc677bc865a2af022fd7081e856d
martyTF <69526651+martyTF@users.noreply.github.com>
2024-03-03 13:04:42 +0100

    Delete .config directory

--- .config/fish/functions/update.fish
+++
@@ -1,7 +0,0 @@
-function update
-sudo dnf update -y rpmfusion-nonfree-release rpmfusion-free-release fedora-repos nobara-repos --refresh && sudo dnf distro-sync --refresh
-sudo dnf update -y --refresh
-flatpak update -y
-am update
-doom sync
-end
