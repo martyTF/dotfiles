d40b5b54d46278d4de8cf856764a704df08a89a2
martyTF <marty_TF@protonmail.com>
2024-03-26 20:32:44 +0100

    hi2

--- configure.sh
+++ configure.sh
@@ -7,13 +7,23 @@ case $SHELL in
     *)
         chsh -s $(which fish)
         ;;
-esac
+    esac
 
-# Theming
+# THEMING
 # Install Catppuccin Mocha Rosewater GTK Theme for flatpak
-wget https://github.com/catppuccin/gtk/releases/download/v0.7.1/Catppuccin-Mocha-Standard-Rosewater-Dark.zip
-unzip -o -d ~/.themes/ Catppuccin-Mocha-Standard-Rosewater-Dark.zip
-rm Catppuccin-Mocha-Standard-Rosewater-Dark.zip
+ls ~/.themes 2> /dev/null && ISTHEME=yes || ISTHEME=no
+case $ISTHEME in
+    no)
+        mkdir ~/.themes
+        wget https://github.com/catppuccin/gtk/releases/download/v0.7.1/Catppuccin-Mocha-Standard-Rosewater-Dark.zip
+        unzip -o -d ~/.themes/ Catppuccin-Mocha-Standard-Rosewater-Dark.zip
+        rm Catppuccin-Mocha-Standard-Rosewater-Dark.zip
+        ;;
+    yes)
+        echo "bruh"
+        ;;
+    esac
+
 
 # Flatpak theming
 sudo flatpak override --filesystem=~/.themes
@@ -23,10 +33,34 @@ sudo flatpak override --filesystem=~/.config/gtk-4.0
 sudo flatpak override --filesystem=~/.local/share/fonts
 sudo flatpak override --env=GTK_THEME=Catppuccin-Mocha-Standard-Rosewater-Dark:dark
 
+# DOTFILES
 # Install dotfiles
-git clone https://github.com/martyTF/dotfiles ~/.dotfiles
+ls ~/.dotfiles 2> /dev/null && ISDOTFILES=yes || ISDOTFILES=no
+case $ISDOTFILES in
+    no)
+        git clone https://github.com/martyTF/dotfiles ~/.dotfiles
+        ;;
+    yes)
+        cd ~/.dotfiles
+        git pull
+        cd
+        ;;
+    esac
 
 
 # Stow dotfiles
-for i in $(ls ~/.dotfiles/.config/); do rm -r ~/.config/$i; done; cd ~/.dotfiles; stow .
-cd
+ls ~/.config-bak 2> /dev/null && ISSTOW=yes || ISSTOW=no
+case $ISSTOW in
+    no)
+        mkdir ~/.config-bak
+        for i in $(ls ~/.dotfiles/.config/); do mv ~/.config/$i ~/.config-bak/$i; done
+        cd ~/.dotfiles
+        stow .
+        cd
+        ;;
+    yes)
+        cd ~/.dotfiles
+        stow .
+        cd
+        ;;
+    esac
--- install-arch.sh
+++ install-arch.sh
@@ -1,25 +1,3 @@
-#!/usr/bin/env bash
-
-# Arch Linux - pacman
-sudo pacman -S --noconfirm git stow emacs flatpak firefox fish lib32-amdvlk steam wofi hyprland waybar atuin ranger gnome-keyring btop fuse nautilus nwg-look mpv neofetch pavucontrol xdg-desktop-portal-gtk kdeconnect swaylock wget
-
-# Arch Linux - install yay
-case $(which yay) in
-    /usr/bin/yay)
-        echo "yay already installed, skipping"
-        ;;
-    *)
-        mkdir ~/git
-         git clone https://aur.archlinux.org/yay.git ~/git/yay
-         cd ~/git/yay
-         makepkg -is --noconfirm
-         ;;
-    esac
-cd
-
-# Arch Linux - yay
-for i in swww waypaper grimshot protonmail-bridge cava asciiquarium-transparent-git catppuccin-gtk-theme-mocha clipman nerdfetch bunnyfetch fastfetch unzip steamtinkerlaunch fd ripgrep shellcheck tidy stylelint js-beautify mako; do pacman -Qm | grep "^$i " && echo "$i is installed, skipping" || yay -S --noconfirm $i; done
-
 # Universal - install doom emacs
 case config.el in
     $(ls $HOME/.config/doom))
@@ -54,7 +32,7 @@ flatpak install -y flathub org.signal.Signal dev.vencord.Vesktop com.github.iwal
 
 # Universal - am
 am install feishin
-
+am install jan
 
 # Configuration
 
