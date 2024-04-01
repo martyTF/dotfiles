3a58e8e3062fc639b983361bf3a4bb2c0235e074
martyTF <marty_TF@protonmail.com>
2024-04-01 09:49:51 +0200

    oops shoulda been yay

--- install-arch.sh
+++ install-arch.sh
@@ -2,7 +2,7 @@
 
 # Arch Linux - pacman
 sudo pacman -Syu --noconfirm
-sudo pacman -S --noconfirm git stow emacs flatpak firefox fish lib32-amdvlk steam wofi hyprland waybar atuin ranger gnome-keyring btop fuse nautilus nwg-look mpv neofetch pavucontrol xdg-desktop-portal-gtk kdeconnect swaylock wget less ethtool onefetch treedome-bin
+sudo pacman -S --noconfirm git stow emacs flatpak firefox fish lib32-amdvlk steam wofi hyprland waybar atuin ranger gnome-keyring btop fuse nautilus nwg-look mpv neofetch pavucontrol xdg-desktop-portal-gtk kdeconnect swaylock wget less ethtool onefetch
 
 # Arch Linux - install yay
 case $(which yay) in
@@ -20,5 +20,5 @@ cd
 
 # Arch Linux - yay
 sudo chmod +s $(which yay)
-for i in swww waypaper grimshot protonmail-bridge cava asciiquarium-transparent-git catppuccin-gtk-theme-mocha clipman nerdfetch bunnyfetch fastfetch unzip steamtinkerlaunch fd ripgrep shellcheck tidy stylelint js-beautify mako nwg-look protontricks light; do pacman -Qm | grep "^$i " && echo "$i is installed, skipping" || yay -S --noconfirm $i; done
+for i in swww waypaper grimshot protonmail-bridge cava asciiquarium-transparent-git catppuccin-gtk-theme-mocha clipman nerdfetch bunnyfetch fastfetch unzip steamtinkerlaunch fd ripgrep shellcheck tidy stylelint js-beautify mako nwg-look protontricks light treedome-bin; do pacman -Qm | grep "^$i " && echo "$i is installed, skipping" || yay -S --noconfirm $i; done
 sudo chmod -s $(which yay)
